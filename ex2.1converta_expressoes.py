1ec394788903db822733f2ff2d657bc6c2f5e00a
Gabriel Macedo <gabrimac11@gmail.com>
2026-05-19 07:00:29 -0300

    Add files via upload

---
+++ ex2.1converta_expressoes.py
@@ -0,0 +1,7 @@
+exp_1 = 10 + 20 * 30
+exp_2 = (4**2) / 30
+exp_3 = (9**2) * 6-1
+
+print(f'Resultado da expressão 1 é: {exp_1}')
+print(f'Resultado da expressão 2 é: {exp_2:.3f}')
+print(f'Resultado da expressão 3 é: {exp_3}')
